89b9ed5cac171fe1186ab8401954d1948eca1b45
avcton <avcton@gmail.com>
2024-06-05 11:04:59 +0500

    .gitignore setup

---
+++ scripts/cleanMD.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Read the content of the file
+content=$(cat)
+
+# Check if the frontmatter contains 'public: true'
+if echo "$content" | grep -q '^public: true'; then
+    # Pass the file through unchanged
+    echo "$content"
+else
+    # Output an empty file
+    echo ""
+fi
