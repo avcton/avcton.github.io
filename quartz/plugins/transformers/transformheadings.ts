24f81281d1b83d32c2f69dce67a5e73df995bda7
avcton <avcton@gmail.com>
2024-06-16 14:46:19 +0500

    Quartz sync: Jun 16, 2024, 2:46 PM

--- quartz/plugins/transformers/transformheadings.ts
+++ quartz/plugins/transformers/transformheadings.ts
@@ -1,5 +1,5 @@
-import { QuartzTransformerPlugin } from "../types"
-import { visit } from "unist-util-visit"
+import { QuartzTransformerPlugin } from "../types";
+import { visit } from "unist-util-visit";
 
 export const TransformHeadings: QuartzTransformerPlugin = () => {
   return {
@@ -7,39 +7,55 @@ export const TransformHeadings: QuartzTransformerPlugin = () => {
     markdownPlugins() {
       return [() => {
         return (tree, file) => {
-          // Use unist-util-visit to traverse the tree and find heading nodes
+          let hasH1Heading = false;
+
+          // First pass: Check if there's at least one H1 heading
           visit(tree, "heading", (node) => {
-            // Check the depth of the heading node
-            switch (node.depth) {
-              case 1:
-                node.depth = 2; // Convert H1 to H2
-                break;
-              case 2:
-                node.depth = 3; // Convert H2 to H3
-                break;
-              case 3:
-                node.depth = 4; // Convert H3 to H4
-                break;
-              case 4:
-                node.depth = 5; // Convert H4 to H5
-                break;
-              case 5:
-                node.depth = 6; // Convert H5 to H6
-                break;
-              case 6:
-                // Convert H6 to strong (bold) text
-                node.type = 'strong';
-                node.depth = null; // Remove depth information
-                node.children = [
-                  { type: 'text', value: node.children[0].value + ':' } // Append colon
-                ];
-                break;
-              default:
-                break;
+            if (node.depth === 1) {
+              hasH1Heading = true;
             }
-          })
-        }
-      }]
+          });
+
+          // If there's at least one H1 heading, transform the headings
+          if (hasH1Heading) {
+            visit(tree, "heading", (node) => {
+              switch (node.depth) {
+                case 1:
+                  node.depth = 2; // Convert H1 to H2
+                  break;
+                case 2:
+                  node.depth = 3; // Convert H2 to H3
+                  break;
+                case 3:
+                  node.depth = 4; // Convert H3 to H4
+                  break;
+                case 4:
+                  node.depth = 5; // Convert H4 to H5
+                  break;
+                case 5:
+                  node.depth = 6; // Convert H5 to H6
+                  break;
+                case 6:
+                  // Convert H6 to strong (bold) text
+                  node.type = 'paragraph';
+                  node.children = [
+                    {
+                      type: 'strong',
+                      children: node.children,
+                    },
+                    {
+                      type: 'text',
+                      value: ':',
+                    },
+                  ];
+                  break;
+                default:
+                  break;
+              }
+            });
+          }
+        };
+      }];
     }
-  }
-}
+  };
+};
