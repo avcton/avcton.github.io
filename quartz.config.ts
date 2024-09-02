3bf2fd3330298658efb08e11acaf424e9f76c8d6
avcton <avcton@gmail.com>
2024-09-02 21:09:05 +0500

    updated dark background theme

--- quartz.config.ts
+++ quartz.config.ts
@@ -38,7 +38,8 @@ const config: QuartzConfig = {
           highlight: "rgba(143, 159, 169, 0.15)",
         },
         darkMode: {
-          light: "#1a1915",
+          // light: "#1a1915",
+          light: "#171717",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
