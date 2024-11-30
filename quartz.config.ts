7513580c2ccf917739e84b1bbc5b368a5b96da50
avcton <avcton@gmail.com>
2024-11-30 18:57:13 +0500

    updated toc depth to 5

--- quartz.config.ts
+++ quartz.config.ts
@@ -75,7 +75,7 @@ const config: QuartzConfig = {
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown({ linkHeadings: true }),
-      Plugin.TableOfContents(),
+      Plugin.TableOfContents({maxDepth: 5}),
       Plugin.CrawlLinks({ markdownLinkResolution: "relative" }),
       Plugin.Description(),
       Plugin.HardLineBreaks(),
