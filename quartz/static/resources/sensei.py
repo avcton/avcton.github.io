ae43869b22c950af1a192e79382284684f8ea213
avcton <avcton@gmail.com>
2024-12-25 13:24:28 +0500

    updated resources

--- quartz/static/resources/sensei.py
+++ quartz/static/resources/sensei.py
@@ -807,6 +807,73 @@ def main():
     ns.Simulator.Destroy()
              """
          },
+        {'title': 'Cisco - Configure DHCP',
+         'code':
+             """
+Router> enable
+Router# confugure terminal
+
+Router(config)# ip dhcp pool [LABEL]
+
+# To assign IPs starting from 192.168.1.2
+Router(dhcp-config)# network 192.168.1.2 255.255.255.0
+Router(dhcp-config)# default-router 192.168.1.1
+Router(dhcp-config)# exit
+
+# To reserve any ip or ip-range
+Router(config)# ip dhcp excluded-address 192.168.1.4 192.168.1.7
+             """
+         },
+        {'title': 'Cisco - Configure RIP',
+         'code':
+             """
+Router> enable
+Router# configure terminal
+
+Router(config)# router rip
+
+Router(config-router)# network 192.168.*.*
+Router(config-router)# network 172.0.0.*
+Router(config-router)# exit
+
+Router(config)# exit
+             """
+         },
+        {'title': 'Cisco - Configure OSPF',
+         'code':
+             """
+Router> enable
+Router# configure terminal
+
+Router(config)# router ospf [Process No.] # Example: router ospf 1
+
+# Example Usage:
+# network [ip to advertize] [complement of its subnet] area [area no.]
+Router(config-router)# network 192.168.*.* 0.0.0.255 area 0
+Router(config-router)# network 172.0.0.* 0.0.255.255 area 0
+Router(config-router)# exit
+
+Router(config)# exit
+             """
+         },
+        {'title': 'Cisco - Configure BGP',
+         'code':
+             """
+Router> enable
+Router# configure terminal
+
+Router(config)#router bgp [AS#] # Example: router bgp 1
+
+# Example Usage:
+# neighbor [neighbor ip] remote-as [AS# of Neighbor]
+# network [ip to advertize] mask [subnet mask of ip]
+Router(config-router)# neighbor 172.16.0.2 remote-as 71
+Router(config-router)# network 10.0.0.0 mask 255.0.0.0
+Router(config-router)# exit
+
+Router(config)#do write
+             """
+         }
         # {'title': '',
         #  'code':
         #      """
